7f80760ef60b69ace2fe331526c096fb3de6925d
linuxyh <yonux@yahoo.co.kr>
2016-07-17 20:36:03 +0900

    Asia/ Seoul 변경

--- djgs/settings.py
+++ djgs/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Seoul'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
